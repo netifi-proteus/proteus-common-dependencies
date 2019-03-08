8eed415f2821dbff27cc90e7351797b02bf4a9d0
Oleh Dokuka <shadowgun@i.ua>
2019-03-09 00:32:01 +0200

    sets path to local settings

--- ci/travis.sh
+++ ci/travis.sh
@@ -9,25 +9,25 @@ echo -e "TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST"
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     # Pull Request
     echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
-    ./mvnw clean install
+    ./mvnw -s ./settings.xml clean install
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "develop" ] && [ "$TRAVIS_TAG" == "" ]; then
     # Develop Branch
     echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
-    ./mvnw clean install artifactory:publish -Drevision-prefix=BUILD-SNAPSHOT
+    ./mvnw -s ./settings.xml clean install artifactory:publish -Drevision-prefix=BUILD-SNAPSHOT
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [[ "$TRAVIS_BRANCH" == release/* ]] && [ "$TRAVIS_TAG" == "" ]; then
     # Release Branch
     echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']'
-    ./mvnw clean install artifactory:publish -Drevision-prefix=RC
+    ./mvnw -s ./settings.xml clean install artifactory:publish -Drevision-prefix=RC
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
     # Master Branch
     echo -e 'Build Master for Release => Branch ['$TRAVIS_BRANCH']'
-    ./mvnw clean install
+    ./mvnw -s ./settings.xml clean install
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
     # Tag
     echo -e 'Build Tag for Release => Tag ['$TRAVIS_TAG']'
-    ./mvnw clean install artifactory:publish -Drevision-prefix=RELEASE
+    ./mvnw -s ./settings.xml clean install artifactory:publish -Drevision-prefix=RELEASE
 else
     # Feature Branch
     echo -e 'Build Branch => Branch ['$TRAVIS_BRANCH']'
-    ./mvnw clean install artifactory:publish -Drevision-prefix=BUILD-SNAPSHOT
+    ./mvnw -s ./settings.xml clean install artifactory:publish -Drevision-prefix=BUILD-SNAPSHOT
 fi
