ac13da7816db2416294833a57c7aa5d8fc959552
Oleh Dokuka <shadowgun@i.ua>
2019-03-17 15:02:52 +0200

    fixes script

--- ci/travis.sh
+++ ci/travis.sh
@@ -14,14 +14,14 @@ elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "develop" ] 
     # Develop Branch
     echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
     ./mvnw -s ./settings.xml clean install deploy -Drevision-prefix=BUILD-SNAPSHOT
-elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [[ "$TRAVIS_BRANCH" =~ ^release\/.*$ ]] && ["$TRAVIS_TAG" == "" ]; then
+elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [[ "$TRAVIS_BRANCH" == release/* ]] && [ "$TRAVIS_TAG" == "" ]; then
     # Release Branch
     echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']'
     ./mvnw -s ./settings.xml clean install deploy -Drevision-prefix=RC
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
     # Master Branch
     echo -e 'Build Master for Release => Branch ['$TRAVIS_BRANCH']'
-    ./mvnw -s ./settings.xml clean install
+    ./mvnw -s ./settings.xml clean install deploy -Drevision-prefix=BUILD-SNAPSHOT
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
     # Tag
     echo -e 'Build Tag for Release => Tag ['$TRAVIS_TAG']'
